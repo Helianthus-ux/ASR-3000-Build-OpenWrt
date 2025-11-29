0cb9d96836ab7fe5012699e5a79a4a0a7f01f9fa
Helianthus <shidaqeng@gmail.com>
2025-11-29 22:56:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.60.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify default SSID
+sed -i "s/ASR3000_2.4G/Breakwa11/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ASR3000_5G/Breakwa11 ax/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
